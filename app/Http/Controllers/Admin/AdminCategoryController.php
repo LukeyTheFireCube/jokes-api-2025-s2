8f8acd1352b208883b0328436ad7980b3742ed55
Ady G <ady.g.coder@gmail.com>
2025-07-29 12:03:45 +0800

    wip (admin/categories): start of category admin
    - add edit method
    - add update method
    - add delete method
    - add destroy method

--- app/Http/Controllers/Admin/AdminCategoryController.php
+++ app/Http/Controllers/Admin/AdminCategoryController.php
@@ -19,7 +19,7 @@ class AdminCategoryController extends Controller
         $validated = $request->validate([
             'perPage' => ['integer', 'nullable'],
             'page' => ['integer', 'nullable'],
-            'search'=> ['nullable', 'string', 'max:32'],
+            'search' => ['nullable', 'string', 'max:32'],
         ]);
 
         $perPage = $validated['perPage'] ?? 12;
@@ -27,7 +27,7 @@ class AdminCategoryController extends Controller
         $search = $validated['search'] ?? '';
 
         // Get the categories
-        $categories = Category::where('title', 'like', '%'.$search.'%')
+        $categories = Category::where('title', 'like', '%' . $search . '%')
             ->orderBy('title')
             ->withCount('jokes')
             ->paginate($perPage, ['*'], 'page', $page);
@@ -37,9 +37,9 @@ class AdminCategoryController extends Controller
 
         // return view
         return view('admin.categories.index')
-            ->with('categories',$categories)
-            ->with('trashCount',$trashCount)
-            ->with('search',$search);
+            ->with('categories', $categories)
+            ->with('trashCount', $trashCount)
+            ->with('search', $search);
     }
 
     /**
@@ -72,38 +72,81 @@ class AdminCategoryController extends Controller
         $category = Category::create($validated);
 
         return redirect()->route('admin.categories.index')
-            ->with('success', 'Category added successfully');
+            ->with('success', "Category {$category->title} added successfully");
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Category $category): View
     {
-        //
+        $categoryId = $category->id;
+
+        $category = Category::where('id', $categoryId)
+            ->withCount('jokes')
+            ->first();
+
+        $jokes = $category->jokesByDateAddedDesc()->limit(5)->get();
+
+        return view('admin.categories.show')
+            ->with('jokes', $jokes)
+            ->with('category', $category);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Category $category): View
     {
-        //
+        return view('admin.categories.edit')
+            ->with('category', $category);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Category $category): RedirectResponse
+    {
+        $validated = $request->validate([
+            'title' => [
+                'required',
+                'string',
+                'min:4', 'max:32',
+                'unique:categories,title',
+            ],
+            'description' => [
+                'nullable',
+                'string',
+                'min:16', 'max:255',
+            ],
+        ]);
+
+        $category->update($validated);
+
+        return redirect()->route('admin.categories.index')
+            ->with('success', "Category {$category->title} updated successfully");
+    }
+
+    /**
+     * Verify the removal of the specified category from storage.
+     */
+    public function delete(Category $category): View
     {
         //
+        return view('admin.categories.delete')
+            ->with('category', $category);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Category $category)
     {
-        //
+        $categoryTitle = $category->title;
+
+        $category->delete();
+
+        return redirect()->route('admin.categories.index')
+            ->with('success', "Category {$categoryTitle} successfully");
     }
 }
