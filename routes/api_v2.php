58f489da99562adb3d5d4ed8abb895eaf2ad9925
Ady G <ady.g.coder@gmail.com>
2025-08-05 12:43:10 +0800

    feat(api/v2): start of v2

---
+++ routes/api_v2.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Api\v1\AuthController as AuthControllerV1;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/**
+ * API Version 2 Routes
+ */
+
+/**
+ * User API Routes
+ * - Register, Login (no authentication)
+ * - Profile, Logout, User details (authentication required)
+ */
+
+Route::prefix('auth')
+    ->group(function () {
+        Route::post('register', [AuthControllerV1::class, 'register']);
+        Route::post('login', [AuthControllerV1::class, 'login']);
+
+        Route::get('profile', [AuthControllerV1::class, 'profile'])
+            ->middleware(['auth:sanctum',]);
+        Route::post('logout', [AuthControllerV1::class, 'logout'])
+            ->middleware(['auth:sanctum',]);
+
+    });
+
