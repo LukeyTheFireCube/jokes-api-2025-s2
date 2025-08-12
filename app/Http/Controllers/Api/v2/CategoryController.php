6a5b85a333af7b3a695e81bf116543edd63f7f34
Ady G <ady.g.coder@gmail.com>
2025-08-12 10:39:16 +0800

    test(categories): Create category pest test
    - index
    - show
    - create
    
    ToDo:
    - update
    - destroy
    - trash index
    - trash recover all
    - trash recover 1
    - trash remove all
    - trash remove 1

--- app/Http/Controllers/Api/v2/CategoryController.php
+++ app/Http/Controllers/Api/v2/CategoryController.php
@@ -10,7 +10,9 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the Categories.
+     *
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
@@ -19,7 +21,10 @@ class CategoryController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created Category in storage.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
@@ -34,20 +39,27 @@ class CategoryController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified Category.
+     *
+     * @param string $id
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show(string $id)
     {
         $category = Category::whereId($id)->get();
 
-        if (count($category)===0) {
+        if (count($category) === 0) {
             return ApiResponse::error($category, "Category not found", 404);
         }
         return ApiResponse::success($category, "Category retrieved");
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the specified Category in storage.
+     *
+     * @param Request $request
+     * @param string $id
+     * @return void
      */
     public function update(Request $request, string $id)
     {
@@ -55,12 +67,59 @@ class CategoryController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified Category from storage.
+     *
+     * @param string $id
+     * @return void
      */
     public function destroy(string $id)
     {
         //
     }
 
+    /**
+     * Show all soft deleted Categories
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function trash(Request $request)
+    {
+    }
 
+    /**
+     * Recover all soft deleted categories from trash
+     *
+     * @return void
+     */
+    public function recoverAll()
+    {
+    }
+
+    /**
+     * Remove all soft deleted categories from trash
+     *
+     * @return void
+     */
+    public function removeAll()
+    {
+    }
+    /**
+     * Recover specified soft deleted category from trash
+     *
+     * @param string $id
+     * @return void
+     */
+    public function recoverOne(string $id)
+    {
+    }
+    /**
+     * Remove specified soft deleted category from trash
+     *
+     * @param string $id
+     * @return void
+     */
+    public function removeOne(string $id)
+    {
+    }
 }
